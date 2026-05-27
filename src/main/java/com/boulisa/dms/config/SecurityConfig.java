f63ef9cdee3b272855de4c52f7f3f2141deedf9d
Tuhami <tuhami.vc@gmail.com>
2026-05-28 01:28:13 +0300

    chore(security): configure password encoding and global security filter chain

---
+++ src/main/java/com/boulisa/dms/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.boulisa.dms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)  {
+        http.csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+
+}
